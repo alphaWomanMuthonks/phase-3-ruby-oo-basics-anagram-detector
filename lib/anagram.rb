e8e60ea1c3b16a083408a50add1fbd0b56eb737f
Joy Muthoni Malanga <joyalphamuthoni@gmail.com>
2022-10-22 19:07:07 +0300

    sorting characters in an array

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)   #initialize
+        @word = word
+        "listen"
+    end
+    def match array            #method match that checks the array's word characters and compares them with the initialised word ,if not matching,it sorts the characters to match       
+        array.select do |word|
+        (word.chars).sort == (@word.chars).sort 
+        end
+    end
+
+
+
+end
